19a6f0562a48e0f18f70c2d9a5a040505601e908
SnO2WMaN <sno2wman.official@gmail.com>
2020-01-25 02:51:25 +0900

    🌐 i18n関連を設定

---
+++ src/locales/i18n.ts
@@ -0,0 +1,21 @@
+import i18n from 'i18next'
+import i18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
+import { initReactI18next } from 'react-i18next'
+
+import en from './en/translation.json'
+import ja from './ja/translation.json'
+
+const resources = { en, ja }
+
+i18n
+  .use(i18nextBrowserLanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    debug: true,
+    keySeparator: false,
+    interpolation: { escapeValue: false },
+  })
+
+export default i18n
