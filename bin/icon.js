6b1e8ce0e30d070c4397596750bd5678cbca4308
SnO2WMaN <sno2wman.official@gmail.com>
2020-02-06 08:43:57 +0900

    🐛 アイコン生成の時にパス解決が失敗していたのを修正

--- bin/icon.js
+++ bin/icon.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
+const path = require('path')
 const sharp = require('sharp')
 
 const resizeTo = [72, 96, 128, 144, 152, 192, 384, 512]
-const filePath = size => `public/icon${size ? `-${size}x${size}` : ''}.png`
 
 ;(async () => {
   await Promise.all([
     resizeTo.map(size =>
-      sharp(filePath())
+      sharp(path.resolve('public', 'icon.png'))
         .resize(size, size)
-        .toFile(filePath(size))
+        .toFile(path.resolve('public', `icon-${size}x${size}.png`))
     ),
   ])
 })()
